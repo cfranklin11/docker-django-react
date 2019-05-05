652985e49a7d8c1c7437a356dfd8b5a1ae5ba3e0
Craig Franklin <craigjfranklin@gmail.com>
2019-05-05 18:33:06 +1000

    Add text input

--- frontend/hello-world/src/App.js
+++ frontend/hello-world/src/App.js
@@ -5,20 +5,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <label for='char-input'>How many characters does</label>
+      <input id='char-input' type='text' />
+      <button>have?</button>
     </div>
   );
 }
