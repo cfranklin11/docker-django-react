821ee760a8503361f71f1fac26ba72c0d2d365f2
Craig Franklin <craigjfranklin@gmail.com>
2019-05-05 19:59:27 +1000

    Connect app component to backend API

--- backend/hello_world/settings.py
+++ backend/hello_world/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "8lj(bgx5_*o%d1$u-#gco+ioorntjjlybb#i1+rl-il0jb=)d7"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["backend"]
 
 
 # Application definition
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,28 @@
 import React from 'react';
 import axios from 'axios';
-import logo from './logo.svg';
 import './App.css';
 
 function handleSubmit(event) {
   const text = document.querySelector('#char-input').value
 
   axios
-    .get(`/char_count?text=${text}`).then(data => {
-      document.querySelector('#char-count').value = `${data.count} characters!`
+    .get(`/char_count?text=${text}`).then(({data}) => {
+      document.querySelector('#char-count').textContent = `${data.count} characters!`
     })
     .catch(err => console.log(err))
 }
-
-
 
 function App() {
   return (
     <div className="App">
       <div>
-        <label for='char-input'>How many characters does</label>
+        <label htmlFor='char-input'>How many characters does</label>
         <input id='char-input' type='text' />
-        <button>have?</button>
+        <button onClick={handleSubmit}>have?</button>
       </div>
 
       <div>
-        <h3 id='char-count' onclick={handleSubmit}></h3>
+        <h3 id='char-count'></h3>
       </div>
     </div>
   );
