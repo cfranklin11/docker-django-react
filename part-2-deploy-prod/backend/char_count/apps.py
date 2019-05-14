1e9145a749a4a3f83f5e5b9ffcc8bf52f8762474
Craig Franklin <cfranklin@marketplacer.com>
2019-05-14 10:59:07 +1000

    Create folder for prod deployment post

---
+++ part-2-deploy-prod/backend/char_count/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CharCountConfig(AppConfig):
+    name = 'char_count'
---
+++ part-2-deploy-prod/backend/hello_world/settings/development.py
@@ -0,0 +1,12 @@
+from hello_world.settings.base import *
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "8lj(bgx5_*o%d1$u-#gco+ioorntjjlybb#i1+rl-il0jb=)d7"
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ["backend"]
