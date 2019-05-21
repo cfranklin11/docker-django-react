5d1b5cd0bd5ab9cc38c8f0c49da01e5c2efd8a00
Craig Franklin <craigjfranklin@gmail.com>
2019-05-21 21:18:44 +1000

    Fix django settings

--- backend/hello_world/urls.py
+++ backend/hello_world/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from django.views.generic import TemplateView
 from char_count.views import char_count
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("char_count", char_count, name="char_count"),
+    re_path(".*", TemplateView.as_view(template_name="index.html")),
 ]
