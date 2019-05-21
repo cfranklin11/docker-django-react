142787ee8493bcd9b4201132e5031da42dfff4ab
Craig Franklin <cfranklin@marketplacer.com>
2019-05-21 11:26:54 +1000

    Update BASE_DIR

--- backend/hello_world/settings/base.py
+++ backend/hello_world/settings/base.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"))
 
 # Application definition
 
